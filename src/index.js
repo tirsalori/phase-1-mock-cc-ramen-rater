1a067dd877150df0a45ffe7e27ed7c021220b4c8
Tirsa St. Fort <lifelovelori@gmail.com>
2023-05-26 13:45:48 -0500

    Complete lab with advanced deliverables

--- src/index.js
+++ src/index.js
@@ -1,29 +1,41 @@
 // write your code here
+
+
 document.addEventListener("DOMContentLoaded", () => {
     fetch('http://localhost:3000/ramens')
         .then((response) => response.json())
         .then((data) => data.forEach(ramenObj => loadRamens(ramenObj)))
+
 })
 
+const detailsDiv = document.getElementById("ramen-detail")
+const h2 = detailsDiv.querySelector(".name")
+const h3 = detailsDiv.querySelector(".restaurant")
+const ramenImg = detailsDiv.querySelector(".detail-image")
+const span = document.getElementById("rating-display")
+const p = document.getElementById("comment-display")
+const menuDiv = document.getElementById("ramen-menu")
 function loadRamens(ramenObj){
     const menuDiv = document.getElementById("ramen-menu")
-    const detailsDiv = document.getElementById("ramen-detail")
     const img = document.createElement("img")
     img.setAttribute("src", ramenObj.image)
     menuDiv.appendChild(img)
     img.addEventListener("click", () => {
-        const h2 = detailsDiv.querySelector(".name")
-        const h3 = detailsDiv.querySelector(".restaurant")
-        const ramenImg = detailsDiv.querySelector(".detail-image")
-        const span = document.getElementById("rating-display")
-        const p = document.getElementById("comment-display")
         h2.innerText = ramenObj.name
         h3.innerText = ramenObj.restaurant
         ramenImg.src = ramenObj.image
         span.innerText = ramenObj.rating
         p.innerText = ramenObj.comment 
     })
+    if (h2.innerText === "Insert Name Here") {
+        h2.innerText = ramenObj.name
+        h3.innerText = ramenObj.restaurant
+        ramenImg.src = ramenObj.image
+        span.innerText = ramenObj.rating
+        p.innerText = ramenObj.comment
+    }
 }
+
 
 const form = document.getElementById("new-ramen")
 form.addEventListener("submit", e => {
@@ -37,3 +49,20 @@ form.addEventListener("submit", e => {
     }
     loadRamens(newRamenObj)
 })
+
+const editForm = document.getElementById("edit-ramen")
+editForm.addEventListener("submit", e => {
+    e.preventDefault()
+    span.innerText = e.target[0].value
+    p.innerText = e.target[1].value
+})
+
+const deleteBtn = document.getElementById("delete-btn")
+deleteBtn.addEventListener("click", () => {
+    ramenImg.src = "./assets/image-placeholder.jpg"
+    h2.innerText = ""
+    h3.innerText = ""
+    ramenImg.src = ""
+    span.innerText = ""
+    p.innerText = ""
+}) 
